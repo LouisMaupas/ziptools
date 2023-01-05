1ea24a5432f184b9b6b2523297b6154cffdcfb76
LouisMaupas <louismaupas@gmail.com>
2023-01-05 11:48:40 +0100

    feat(main) wip front

--- ziptools/main/views.py
+++ ziptools/main/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
 
 # Create your views here.
 def index(requests):
+    template = loader.get_template('index.html')
+    global data
+    data = {
+                "form": "test"
+            }
     if requests.GET:
         pass
     elif requests.POST:
         pass
-    return HttpResponse("Salut")
+    return HttpResponse(template.render(data))
